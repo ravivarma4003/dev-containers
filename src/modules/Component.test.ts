1345fea930d4473e53bbe34c0f6e0972ac2dd31f
Brian Wehrle <brian.wehrle@josys.com>
2025-03-24 16:20:37 +0100

    Make generic

---
+++ src/modules/Component.test.ts
@@ -0,0 +1,13 @@
+
+
+describe("Test", () => {
+    beforeEach(() => {
+
+    });
+
+    test("Test Case 1", () => {
+        const name = "foo";
+        const lastName = "bar";
+        expect(name+lastName).toBe("foobar");
+    });
+});
